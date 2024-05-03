c21abcbc9e59b493c40f58d949be8ec656da8b03
Maja Goltermann <maja.goltermann@gmail.com>
2024-05-03 13:28:52 +0200

    n

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    
+    experimental:{
+        assets: true,
+    },
 });
 
 
--- postcss.config.cjs
+++
@@ -1,6 +0,0 @@
-const postcssjitProps = require("postcss-jit-props");
-const OpenProps = require("open-props");
-const postcssNesting = require("postcss-nesting");
-module.exports = {
-    plugins: [postcssNesting, postcssjitProps(OpenProps)],
-};
